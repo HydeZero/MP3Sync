082cfc2918ed681b8aaf5d70c9ebbe30a5059ee2
HydeZero <128327411+HydeZero@users.noreply.github.com>
2024-12-05 20:24:55 -0600

    update ffmpeg & mutagen licenses & implement windows support

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from genericpath import isdir
 import os
 import platform
+import subprocess
 import pdb
 
-# Detect pathlib. If not there, halt.
+if input("show license? [y/N] ").lower() == "y":
+    with open ("LICENSE", "r") as license_text:
+        print(license_text.read())
 
 try:
     from pathlib import PurePath
@@ -18,17 +22,16 @@ if (platform.system() == "Linux"):
 elif (platform.system() == "Windows"):
     home=os.environ['UserProfile'].replace("\\","/")
 
-print(home)
-
 # Get (optional) pydub.
 # TODO: See if conversion works without 
-
 try:
     from pydub import AudioSegment
     isPydub = True
+    print("Please make sure ffmpeg is in C:\\ffmpeg and that C:\\ffmpeg is in. Otherwise you don't need to do anything at all.")
 except Exception as e:
     print("PyDub was either not found or was unable to be imported. MP3 conversion will not be able to be used.")
     print(e)
+
 try:
     from mutagen.flac import FLAC
     from mutagen.mp3 import MP3
@@ -40,13 +43,13 @@ except Exception as e:
 if (input(f"Is this your music library? {home}/Music/ [Y/n]").lower() != "n"):
     musicLibrary = home+"/Music/"
 
-print("Type any directories you want to exclude, separated by commas.")
+print("Type any directories you want to exclude, separated by commas, NO SPACES BETWEEN ITEMS.")
 
-dirToExclude = input().strip().split(",")
+dirToExclude = input().split(",")
 
 print("TYPE THE DIRECTORY WHERE YOUR MP3 PLAYER IS MOUNTED. WE WILL HANDLE EVERYTHING FROM HERE.")
 
-mp3MountDir = input("FULL DIRECTORY TO MP3 PLAYER MOUNT POINT (/using/this/directory/structure/): ")
+mp3MountDir = input("FULL DIRECTORY TO MP3 PLAYER MOUNT POINT (/using/this/directory/structure/). Forward slashes work on windows also.\n> ").replace("\\", "/")
 
 print("Searching for files... Please hold...")
 
@@ -56,7 +59,7 @@ directs = []
 def getDirs(direct):
     for item in os.listdir(direct):
         print(item)
-        if item not in dirToExclude:
+        if not (item in dirToExclude):
             try:
                 if item.split(".")[1] != "???":
                     pass
@@ -70,20 +73,23 @@ curPath = musicLibrary
 
 files = []
 
+fileNum = 1
+
 for director in hostDirects:
     if director == "Playlists":
         continue
     for item in os.listdir(curPath + director):
         curPath = musicLibrary + director
         if item.split(".")[-1] != "jpg":
-            for item2 in os.listdir(curPath + "/" + item):
-                if item2.split(".")[-1] in acceptableExtensions:
-                    print(item2.split(".")[-1])
-                    print(curPath + "/" + item + "/" + item2)
-                    files.append(curPath + "/" + item + "/" + item2)
+            if os.path.isdir(curPath + "/" + item):
+                for item2 in os.listdir(curPath + "/" + item):
+                    if item2.split(".")[-1] in acceptableExtensions:
+                        print(f"Got item {fileNum}")
+                        fileNum += 1
+                        files.append(curPath + "/" + item + "/" + item2)
     curPath = musicLibrary
 
-print(files)
+print("Got files.")
 
 try:
     os.mkdir(mp3MountDir + "/" + "Music")
@@ -101,9 +107,15 @@ if isPydub:
                 
                 path_as_str = os.fspath(file_path)
 
-                artist_dir = path_as_str.split("/")[-3]
+                try:
+                    artist_dir = path_as_str.split("/")[-3]
+                except IndexError:
+                    artist_dir = path_as_str.split("\\")[-3]
                 
-                album_dir = path_as_str.split("/")[-2]
+                try:
+                    album_dir = path_as_str.split("/")[-2]
+                except IndexError:
+                    album_dir = path_as_str.split("\\")[-2]
 
                 if not os.path.isdir(mp3MountDir +  "/" + artist_dir + "/" + album_dir):
                     os.mkdir(mp3MountDir + "/" + artist_dir)
@@ -136,5 +148,8 @@ if isPydub:
 
                 output_metadata["genre"] = source_metadata["GENRE"]
 
-
                 output_metadata.save() # we're done here
+                
+                # Now we take the flac album cover and copy it over to the MP3, making it as complete as possible
+
+                # TODO: Add album cover copying mechanism
